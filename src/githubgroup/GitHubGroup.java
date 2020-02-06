e6fef2d558f7ac5cd103bfc2ffca0869bd1487e3
bradv555 <56845446+bradv555@users.noreply.github.com>
2020-02-06 13:13:06 -0500

    Initial commit

---
+++ src/githubgroup/GitHubGroup.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubgroup;
+
+/**
+ *
+ * @author Brad
+ */
+public class GitHubGroup {
+
+   public static void Hi(){
+       System.out.println("Hello World");
+   }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Hi();
+    }
+    
+}
